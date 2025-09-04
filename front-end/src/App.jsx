bd98094143ddb581a3225c3ad2ce0dc7ce45bbd3
Avinash-2508 <avinash.ponneboina@gmail.com>
2025-09-04 19:35:02 +0530

    Initial commit

---
+++ front-end/src/App.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+import Navbar from './components/navbar/navbar'
+import Home from './pages/Home/Home'
+import Cart from './pages/Cart/Cart'
+import Placeorder from './pages/Placeorder/Placeorder'
+import { Route, Routes } from 'react-router-dom'
+import Footer from './components/Footer/Footer'
+import LoginPopup from './components/LoginPopup/LoginPopup'
+
+const App = () => {
+
+  const [showLogin,setShowlogin] = useState(false)
+  
+  return (
+    <>
+    {showLogin?<LoginPopup setShowlogin={setShowlogin}/>:<></>}
+      <div className='app'>
+        <Navbar setShowlogin={setShowlogin}/>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/cart' element={<Cart/>} />
+          <Route path='/order' element={<Placeorder />} />
+        </Routes>
+      </div>
+      <Footer />
+    </>
+  )
+}
+
+export default App
